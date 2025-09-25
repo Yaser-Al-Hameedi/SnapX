e568b3f93c9e80318455bf2e5cec761866c3197b
Yaser-Al-Hameedi <yaseralhameedi@gmail.com>
2025-09-25 12:44:20 -0400

    chore: add frontend and backend folders

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl md:text-5xl font-bold">SnapX frontend is live</h1>
+    </main>
+  );
+}
